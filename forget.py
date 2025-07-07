1d607fa2f6e06becc4c48ff0f9a5247685e13f8e
Vishal Singh Gusain <innovativecoding1@gmail.com>
2025-07-07 13:15:08 +0530

    Add files via upload

---
+++ forget.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+
+class Forget:
+    def __init__(self,root):
+         self.root=root
+         self.root.title("customer window")
+         self.root.geometry("1265x560+285+230")
+
+
+
+if __name__ =="__main__":
+    root=Tk()
+    obj= Forget(root)
+    root.mainloop()
